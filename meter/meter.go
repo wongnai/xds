e5e2ab723557a20047d386fcb1aac928ea4fb6c2
Manatsawin Hanmongkolchai <manatsawin+git@lmwn.com>
2023-08-09 10:59:57 +0700

    Bump dependencies, upgrade to stable otel, remove node_id metric

--- meter/meter.go
+++ meter/meter.go
@@ -1,10 +1,22 @@
 package meter
 
 import (
+	"go.opentelemetry.io/otel"
+	otelprom "go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/metric/global"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
 func GetMeter() metric.Meter {
-	return global.Meter("k8sxds")
+	return otel.Meter("k8sxds")
+}
+
+func InstallPromExporter() error {
+	promReader, err := otelprom.New()
+	if err != nil {
+		return err
+	}
+	provider := sdkmetric.NewMeterProvider(sdkmetric.WithReader(promReader))
+	otel.SetMeterProvider(provider)
+	return nil
 }
