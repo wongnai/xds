a63fb7915ab702005c4accd87d11c19b09e71d48
Manatsawin Hanmongkolchai <manatsawin@lmwn.com>
2025-06-18 14:54:20 +0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gitignore }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      stdenv = pkgs.stdenv;
+      lib = pkgs.lib;
+      source = gitignore.lib.gitignoreSource self;
+    in rec {
+      packages = {
+        default = pkgs.buildGoModule {
+          name = "xds";
+          src = gitignore.lib.gitignoreSource ./.;
+          vendorHash = "sha256-Z9D/XiYUb1GGrTyjgG6R2BMnsa42zysvlv20YXRBTLo=";
+        };
+        container = pkgs.dockerTools.buildLayeredImage {
+          name = "xds";
+          contents = with pkgs; [ dockerTools.caCertificates ];
+        };
+      };
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ go ];
+        };
+      };
+    }
+  );
+}
